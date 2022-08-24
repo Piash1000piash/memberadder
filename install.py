9af810583e44470644a4263a408353646302137f
Piash1000piash <111903209+Piash1000piash@users.noreply.github.com>
2022-08-24 20:07:34 +0600

    Add files via upload

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("pip3 install pyrogram")
+os.system("pip3 install tgcrypto")
+os.system("pip3 install colorama")
+os.system("pip3 install lolpython")
+os.system("pip3 install requests")
+os.system("pip install pyfiglet")
+
+print("|✓| Installation Done")
